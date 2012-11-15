dbbffb3422cfdb88dc2811c00c20a5b2d9035f4c
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2012-11-15 15:29:14 +0100

    Initial Commit

---
+++ QtWrapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QtWrapper
+//
+//  Created by Richard Moe Gustavsen on 11/15/12.
+//  Copyright (c) 2012 Richard Moe Gustavsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
